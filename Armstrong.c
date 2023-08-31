d7b7312bea45130e979487ca735f112eace7d05b
Sandesh Bhonde <125466708+Sandeshbhonde@users.noreply.github.com>
2023-08-31 22:59:11 +0530

    given number is Armstrong or not

--- Armstrong.c
+++ Armstrong.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 int main() 
 {
-    
     int num, originalNum, remainder, result = 0;
     
     printf("Enter a three-digit integer: ");
@@ -9,17 +8,17 @@ int main()
     
     originalNum = num;
 
-    while (originalNum != 0) {
-        remainder = originalNum % 10;
+    while (originalNum != 0) 
+    {
+            remainder = originalNum % 10;
         
        result += remainder * remainder * remainder;
-
-       originalNum /= 10;
+        
+           originalNum /= 10;
     }
 
     if (result == num)
         printf("%d is an Armstrong number.", num);
-        
     else
         printf("%d is not an Armstrong number.", num);
 
